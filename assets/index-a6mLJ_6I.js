b4e11d4cfdeab5c187387936e37c5ffebc424fe7
Parleneatrick <parleneatrick@gmail.com>
2024-06-24 21:08:27 +0700

    new

--- assets/index-a6mLJ_6I.js
+++ assets/index-a6mLJ_6I.js
@@ -11710,10 +11710,7 @@ qs.defaultProps = {
 qs.defaultStyles = {
   overlay: {
     position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    inset:0,
     backgroundColor: "rgba(0,0,0, 0.6)",
   },
   content: {
